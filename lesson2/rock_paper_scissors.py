0f98d86f7f044fb3c7d594f8bc226494ebfcac81
Blake Pozolo <btpozolo@umich.edu>
2024-04-15 11:51:55 -0700

    rps with bonus features

--- lesson2/rock_paper_scissors.py
+++ lesson2/rock_paper_scissors.py
@@ -3,19 +3,29 @@ import random
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock', ]
 SHORTENED_CHOICES = ['r', 'p', 's', 'l', 'p']
 FULL_CHOICES = [[x, y] for x, y in zip(VALID_CHOICES, SHORTENED_CHOICES)]
-PLAYER_TYPE = ['user', 'computer']
 
-# user_score = 0
-# computer_score = 0
+player_score = {
+        'User':     0,
+        'Computer': 0,
+}
+player_keys = list(player_score.keys())
+
+WINNING_COMBOS = {
+    'rock':     ['scissors', 'lizard'],
+    'paper':    ['rock',     'spock'],
+    'scissors': ['paper',    'lizard'],
+    'lizard':   ['paper',    'spock'],
+    'spock':    ['rock',     'scissors'],
+}
 
 def display_winner(user_choice, computer_choice):
     prompt(f"You chose {user_choice}, computer chose {computer_choice}")
 
     winner = get_winner(user_choice, computer_choice)
 
-    if winner == PLAYER_TYPE[0]:
+    if winner == player_keys[0]:
         prompt("You win!")
-    elif winner == PLAYER_TYPE[1]:
+    elif winner == player_keys[1]:
         prompt("Computer wins!")
     else:
         prompt("Its a tie!")
@@ -26,9 +36,7 @@ def prompt(message):
 def convert_choice(choice):
     if len(choice) == 1:
         for item in FULL_CHOICES:
-            #print(item)
             if item[1] == choice:
-                #print(item[1])
                 choice = item[0]
     return choice
 
@@ -50,7 +58,6 @@ def display_welcome():
         '----------------------------------------------\n')
 
 def play_round():
-
     prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
     prompt(list(FULL_CHOICES))
     choice = get_choice()
@@ -61,28 +68,15 @@ def play_round():
     increment_score(get_winner(choice, computer_choice))
 
 def increment_score(player):
-    if player == PLAYER_TYPE[0]:
-        nonlocal user_score += 1
-    elif player == PLAYER_TYPE[1]:
-        nonlocal computer_score += 1
+    if player in player_score:
+        player_score[player] += 1
 
 def get_winner(user_choice, computer_choice):
-    if ((user_choice == "rock" and (computer_choice == "scissors" or 
-            computer_choice == 'lizard')) or
-        (user_choice == "paper" and (computer_choice == "rock" or 
-            computer_choice == 'spock')) or
-        (user_choice == "scissors" and (computer_choice == "paper" or 
-            computer_choice == 'lizard')) or
-        (user_choice =='lizard' and (computer_choice == 'paper' or 
-            computer_choice == 'spock')) or
-        (user_choice == 'spock' and (computer_choice == 'rock' or 
-            computer_choice == 'scissors'))
-        ):
-        return PLAYER_TYPE[0]
-    elif user_choice == computer_choice:
+    if computer_choice in WINNING_COMBOS[user_choice]:
+        return player_keys[0] # Player wins
+    if user_choice == computer_choice:
         return 'tie'
-    else:
-        return PLAYER_TYPE[1]
+    return player_keys[1] # Computer wins
 
 def play_again():
     prompt("Do you want to play again (y/n)?")
@@ -97,25 +91,30 @@ def play_again():
     return True
 
 def is_grand_winner():
-    return max(user_score, computer_score) >= 3
+    return max(player_score.values()) >= 3
 
 def get_grand_winner():
-    if user_score > computer_score:
-        return PLAYER_TYPE[0]
-    return PLAYER_TYPE[1]
+    return max(player_score, key = player_score.get)
+
+def display_scores():
+    for key, value in player_score.items():
+        prompt(f'{key} has a score of: {value}')
+    print('')
 
 # Main Program
 display_welcome()
 
 while True:
-    user_score = 0
-    computer_score = 0
 
-    while is_grand_winner() != True:
+    while is_grand_winner() is not True:
         play_round()
+        display_scores()
 
-    print(f'The grand winner is {get_grand_winner()}!')
+    print(f'The grand winner is {get_grand_winner()}!\n')
 
     if not play_again():
         prompt('Thank you for playing!')
         break
+
+    for name in player_score:
+        player_score[name] = 0
